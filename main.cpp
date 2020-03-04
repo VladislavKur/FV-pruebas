d527b9cd82d5761242eda05fc7b1e45c8675c3fb
mbg123 <mbg123@alu.ua.es>
2020-03-04 11:31:22 +0100

    cambio_main

--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-
-
 #include "include/config.h"
 #include "ej_modulos/mimodulo.h"
 
-#define kVel 5
+#define kVel 1
 
 int main() {
-  /////////////
-  ////START////
-  /////////////
 
+  MiModulo *mod = new MiModulo();
 
   //Creamos una ventana
-  sf::RenderWindow window(sf::VideoMode(640, 480), "P0. Fundamentos de los Videojuegos. DCCIA");
+  sf::RenderWindow window(sf::VideoMode(1200, 1200), "P0. Fundamentos de los Videojuegos. DCCIA");
 
   //Cargo la imagen donde reside la textura del sprite
   sf::Texture tex;
@@ -29,30 +25,70 @@ int main() {
 
   //Le pongo el centroide donde corresponde
   sprite.setOrigin(75 / 2, 75 / 2);
-  
   //Cojo el sprite que me interesa por defecto del sheet
-  sprite.setTextureRect(sf::IntRect(0 * 75, 0 * 75, 75, 75));
+  sprite.setTextureRect(sf::IntRect(0 * 75, 3 * 75, 75, 75));
 
   // Lo dispongo en el centro de la pantalla
-  sprite.setPosition(320, 240);
+  sprite.setPosition(100, 100);
 
-  float jumpSpeed=0;
-  float yInicial=sprite.getPosition().y;
-  bool saltando=false;
+  int modoReptante = 0; //0: suelo, 1: paredD; 2: techo 3: paredI
+  char direccion = -1; //1: reloj, -1: contrario
 
-  float jumpHeight=75*1.5;
-
-  float deltaTime = 0;
-  sf::Clock clock;
-
-  //////////////////
-  ////BUCLE////////
-  ////////////////
+  //Bucle del juego
   while (window.isOpen()) {
+    if(direccion == -1)
+    modoReptante = 1;
+    //switch movimiento del reptante
+    switch(modoReptante){
+      case 0: //suelo
+          sprite.move(kVel*direccion, 0);
+          if(sprite.getPosition().x == 1100)
+            modoReptante = 3;
+      break;
+      case 1: //paredDerecha
+          sprite.move(0, -kVel*direccion);
+          if(sprite.getPosition().y == 100)
+            modoReptante = 0;
+      break;
+      case 2://Techo
+          sprite.move(-kVel*direccion, 0);
+          if(sprite.getPosition().x == 100)
+            modoReptante = 1;
+      break;
+      case 3://Techo
+          sprite.move(0, kVel*direccion);
+          if(sprite.getPosition().y == 800)
+            modoReptante = 2;
+      break;
+    }
     
-    //////////////
-    ////INPUT/////
-    /////////////
+    sf::Event event;
+    while (window.pollEvent(event)) {
+
+      switch (event.type) {
+        case sf::Event::Closed:
+          window.close();
+        break;
+
+        //Se pulsó una tecla, imprimo su codigo
+        case sf::Event::KeyPressed:
+          //Verifico si se pulsa alguna tecla de movimiento
+          switch (event.key.code) {
+            //Tecla ESC para salir
+          case sf::Keyboard::Escape:
+            window.close();
+            break;
+          
+          //Cualquier tecla desconocida se imprime por pantalla su código
+          default:
+            std::cout << event.key.code << std::endl;
+            break;
+          }
+      }
+    }
+          
+    //Bucle de obtención de eventos
+    /*
     sf::Event event;
     while (window.pollEvent(event)) {
 
@@ -62,13 +98,37 @@ int main() {
       case sf::Event::Closed:
         window.close();
         break;
-
+      
       //Se pulsó una tecla, imprimo su codigo
       case sf::Event::KeyPressed:
 
         //Verifico si se pulsa alguna tecla de movimiento
         switch (event.key.code) {
-          
+
+        //Mapeo del cursor
+        case sf::Keyboard::Right:
+          sprite.setTextureRect(sf::IntRect(0 * 75, 2 * 75, 75, 75));
+          //Escala por defecto
+          sprite.setScale(1, 1);
+          sprite.move(kVel, 0);
+          break;
+
+        case sf::Keyboard::Left:
+          sprite.setTextureRect(sf::IntRect(0 * 75, 2 * 75, 75, 75));
+          //Reflejo vertical
+          sprite.setScale(-1, 1);
+          sprite.move(-kVel, 0);
+          break;
+
+        case sf::Keyboard::Up:
+          sprite.setTextureRect(sf::IntRect(0 * 75, 3 * 75, 75, 75));
+          sprite.move(0, -kVel);
+          break;
+
+        case sf::Keyboard::Down:
+          sprite.setTextureRect(sf::IntRect(0 * 75, 0 * 75, 75, 75));
+          sprite.move(0, kVel);
+          break;
 
         //Tecla ESC para salir
         case sf::Keyboard::Escape:
@@ -80,60 +140,9 @@ int main() {
           std::cout << event.key.code << std::endl;
           break;
         }
-        default: break;
       }
-    }
+    }*/
 
-
-    ////////////
-    ///UPDATE///
-    ////////////
-    deltaTime = clock.restart().asSeconds();
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)){
-      sprite.setTextureRect(sf::IntRect(0 * 75, 2 * 75, 75, 75));
-      //Escala por defecto
-      sprite.setScale(1, 1);
-      sprite.move(0.05, 0);
-    }
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){
-      sprite.setTextureRect(sf::IntRect(0 * 75, 2 * 75, 75, 75));
-          //Escala por defecto
-          sprite.setScale(-1, 1);
-          sprite.move(-0.05, 0);
-    }
-
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)){
-
-          if(!saltando){
-            saltando=true;
-            jumpSpeed = -sqrtf(2.0f * 981.0f * jumpHeight);
-          }
-          
-
-    }
-
-    if(sprite.getPosition().y>yInicial){
-      jumpSpeed=0;
-      sprite.setPosition( sf::Vector2f(sprite.getPosition().x,yInicial) );
-      saltando=false;
-    }
-    
-    if(saltando){
-        
-        jumpSpeed+=981.0f*deltaTime;
-
-        
-
-    }
-    
-    sprite.move(sf::Vector2f(0,jumpSpeed*deltaTime));
-    
-
-    ///////////////
-    /////DRAW/////
-    //////////////
     window.clear();
     window.draw(sprite);
     window.display();
