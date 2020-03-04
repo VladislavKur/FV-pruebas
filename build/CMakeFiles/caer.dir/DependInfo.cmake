3bc678b430f29c5ab84e173cb65a796f5dc5ada4
mbg123 <mbg123@alu.ua.es>
2020-03-04 12:09:56 +0100

    enemigo reptante basico

--- build/CMakeFiles/caer.dir/DependInfo.cmake
+++ build/CMakeFiles/caer.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/andres/Escritorio/FV-pruebas/caer.cpp" "/home/andres/Escritorio/FV-pruebas/build/CMakeFiles/caer.dir/caer.cpp.o"
+  "/home/fv/Desktop/FV-pruebas/caer.cpp" "/home/fv/Desktop/FV-pruebas/build/CMakeFiles/caer.dir/caer.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- build/CMakeFiles/recoger.dir/DependInfo.cmake
+++ build/CMakeFiles/recoger.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/andres/Escritorio/FV-pruebas/recoger.cpp" "/home/andres/Escritorio/FV-pruebas/build/CMakeFiles/recoger.dir/recoger.cpp.o"
+  "/home/fv/Desktop/FV-pruebas/recoger.cpp" "/home/fv/Desktop/FV-pruebas/build/CMakeFiles/recoger.dir/recoger.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
