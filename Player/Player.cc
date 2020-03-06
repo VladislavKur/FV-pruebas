9041908987e60ef26de01d8569d083c997380e09
Vermia <domeofvermia@outlook.com>
2020-03-06 20:20:41 +0100

    COLISIONES CON PLATAFORMASgit add *git add *

--- Player/Player.cc
+++ Player/Player.cc
@@ -3,6 +3,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "../Plataforma/Plataforma.h"
+
+using namespace sf;
 Player::~Player(){
 
 }
@@ -13,10 +15,11 @@ Player::Player(sf::Texture* tex, sf::Vector2u cantidadImagenes, float SwitchTime
     arma=0;
     body.setSize(sf::Vector2f(100.0f,100.0f));
     body.setTexture(tex);
+    
     body.setOrigin(75 / 2, 75 / 2);  // 75 es el tamaño del sprite, cambiar
     body.setPosition(320, 40);//Quitar esto mas adelante
 
-    
+    body.setTextureRect(sf::IntRect(0 * 75, 2 * 75, 75, 75));
     saltos = 1;
     jumpSpeed=0;
     jumpHeight=75*2;
@@ -25,23 +28,57 @@ Player::Player(sf::Texture* tex, sf::Vector2u cantidadImagenes, float SwitchTime
 }
 
 void Player::update(float deltaTime, Plataforma plataforma, Plataforma suelo){
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))moveRight(deltaTime);
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))moveLeft(deltaTime);
+    Vector2f gp = body.getPosition();
+    FloatRect gbb = body.getGlobalBounds();
 
- if(body.getGlobalBounds().intersects(plataforma.getBody().getGlobalBounds()) ||
-   body.getGlobalBounds().intersects(suelo.getBody().getGlobalBounds()) ){
+    coliAbajo.left = gp.x - gbb.width/2 + 25;
+    coliAbajo.top = gp.y + gbb.height/2;
+    coliAbajo.width = gbb.width-50;
+    coliAbajo.height = 6;
+    
+    coliIzquierda.left = gp.x - gbb.width/2+12;
+    coliIzquierda.top = gp.y - gbb.height/2 + 25 ;
+    coliIzquierda.width = gbb.width/2-15;
+    coliIzquierda.height = gbb.height -25;
+
+    coliDerecha.left = gp.x+5;
+    coliDerecha.top = gp.y - gbb.height/2 +25;
+    coliDerecha.width = gbb.width/2 -20;
+    coliDerecha.height = gbb.height -25;
+
+    coliArriba.left = gp.x - gbb.width/2 + 20;
+    coliArriba.top = gp.y-gbb.height/2 +25;
+    coliArriba.width = gbb.width - 40;
+    coliArriba.height = 5;
+
+
+    //Moverse a la derecha si la plataforma lo permite
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)){
+      if(!coliDerecha.intersects(plataforma.getBody().getGlobalBounds()))
+        moveRight(deltaTime);
+      }
+    //Moverse a la izquierda si la plataforma lo permite
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)){
+      if(!coliIzquierda.intersects(plataforma.getBody().getGlobalBounds()))
+        moveLeft(deltaTime);
+      }
+
+//Dejar de caer si toco plataforma
+ if(coliAbajo.intersects(plataforma.getBody().getGlobalBounds()) ||
+   coliAbajo.intersects(suelo.getBody().getGlobalBounds()) ){
     saltos = PU_saltoDoble ? 2 : 1;
     jumpSpeed=0;
    } else 
       jumpSpeed+=981.0f*deltaTime;
 
   //caer
-    if(saltos==0){
-       
-    }
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)){
         saltar();
+    }
+
+    if(coliArriba.intersects(plataforma.getBody().getGlobalBounds())){
+        jumpSpeed=1;
     }
     
     //Caída constante
