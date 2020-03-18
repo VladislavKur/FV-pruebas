830e8b94e316f01e7f91f8b3785e65ae95bdd965
Vermia <domeofvermia@outlook.com>
2020-03-18 17:49:39 +0100

    Bullet (pensaba que lo habia subido pero no)

--- Bullet/Bullet.cc
+++ Bullet/Bullet.cc
@@ -1,6 +1,23 @@
 #include "Bullet.h"
 
+  Bullet* Bullet::bullets[maxBullets];
+  bool Bullet::array_inicializado=false;
+
+
+//Necesario porque si no los punteros apuntan a drecciones aleatorias
+  void Bullet::initBulletArray(){
+    for(int i = 0 ; i < maxBullets ; i++){
+        Bullet::bullets[i]=NULL;
+    }
+  }
+  
+
 Bullet::Bullet(float p_x, float p_y, bool p_facing) : body(){
+    if(!array_inicializado){
+        initBulletArray();
+        array_inicializado=true;
+    }
+
     lifetime=5;
 
     facing=p_facing;
--- Bullet/Bullet.h
+++ Bullet/Bullet.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+#define maxBullets 64
+
 class Bullet
 {
 private:
@@ -11,7 +13,11 @@ public:
     Bullet(float, float, bool);
     ~Bullet() {}
 
-    float lifetime;
+    float lifetime; //Contador que destruye la bala cuando llega a 0
     void update(float deltaTime);
     void draw(sf::RenderWindow& window);
+
+    static Bullet* bullets[maxBullets];
+    static void initBulletArray();
+    static bool array_inicializado;
 };
